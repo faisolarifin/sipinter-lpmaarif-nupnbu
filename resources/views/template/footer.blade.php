8875a01eb9d36a9528d639f080f7b2330efb778c
Ach. Faisol S. Arifin <ach.arifin@jatis.com>
2023-07-23 21:01:01 +0700

    update footer up

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,28 +1,30 @@
 
 <div class="container-fluid footer-up mt-5">
     <div class="row">
-        <div class="container">
-            <div class="row py-4">
-                <div class="col-12 col-sm-7">
-                    <div>
-                        <img src="{{ asset('assets/images/logos/Logo_NU_Putih_PNG.png') }}" width="100" alt="Logo NU">
-                        <p class="mb-0 fs-4">Sistem Administrasi Pendidikan Terpadu (SIAPINTER)</p>
-                        <p class="mb-0">Lembaga Pendidikan Ma'arif NU</p>
-                        <p>Pengurus Besar Nahdlatul Ulama</p>
+        <div class="col">
+            <div class="container">
+                <div class="row py-4">
+                    <div class="col-12 col-sm-7">
+                        <div>
+                            <img src="{{ asset('assets/images/logos/Logo_NU_Putih_PNG.png') }}" width="100" alt="Logo NU">
+                            <p class="mb-0 fs-4">Sistem Administrasi Pendidikan Terpadu (SIAPINTER)</p>
+                            <p class="mb-0">Lembaga Pendidikan Ma'arif NU</p>
+                            <p>Pengurus Besar Nahdlatul Ulama</p>
+                        </div>
+                        <div>
+                            <p class="mb-2">Integrated By.</p>
+                            <img src="{{ asset('assets/images/backgrounds/33ddc3bc2640689.png') }}" width="60" alt="">
+                        </div>
                     </div>
-                    <div>
-                        <p class="mb-2">Integrated By.</p>
-                        <img src="{{ asset('assets/images/backgrounds/33ddc3bc2640689.png') }}" width="60" alt="">
-                    </div>
-                </div>
-                <div class="col-12 col-sm-5">
-                    <div class="address">
-                        <p>Alamat</p>
-                        <p>Lembaga Pendidikan Ma’arif Nahdlatul Ulama Pengurus Besar Nahdlatul Ulama Gedung PBNU II Lt. 2 Jl. Taman Amir Hamzah No. 5 Jakarta Pusat 10320</p>
-                        <p>Telp.</p>
-                        <p>021-3904115</p>
-                        <p>Email</p>
-                        <p>sekretariat@maarifnu.org</p>
+                    <div class="col-12 col-sm-5">
+                        <div class="address">
+                            <p>Alamat</p>
+                            <p>Lembaga Pendidikan Ma’arif Nahdlatul Ulama Pengurus Besar Nahdlatul Ulama Gedung PBNU II Lt. 2 Jl. Taman Amir Hamzah No. 5 Jakarta Pusat 10320</p>
+                            <p>Telp.</p>
+                            <p>021-3904115</p>
+                            <p>Email</p>
+                            <p>sekretariat@maarifnu.org</p>
+                        </div>
                     </div>
                 </div>
             </div>
