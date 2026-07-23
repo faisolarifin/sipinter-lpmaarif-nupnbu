42a3ae062d7f4b241a98383ec128d4502b5647f4
ach.arifin <ach.arifin@jatis.com>
2026-07-23 08:53:13 +0700

    wider form layout for npsnvirtual page matching reference

--- resources/views/auth/npsnvirtual.blade.php
+++ resources/views/auth/npsnvirtual.blade.php
@@ -9,8 +9,8 @@
 
 @section('container')
 <div class="sip-shell">
-    <div class="sip-left">
-        <div class="sip-card">
+    <div class="sip-left sip-left-wide">
+        <div class="sip-card sip-card-wide">
             <a href="{{ route('home') }}" class="sip-back-home">
                 <svg width="13" height="13" viewBox="0 0 24 24" fill="none"><path d="M19 12H5M11 6l-6 6 6 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
                 Kembali ke Beranda
